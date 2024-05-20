e2cf2e9349021cbc1866dd5c88fa10680ab26279
Andre-Francelino <107659656+Andre-Francelino@users.noreply.github.com>
2024-05-20 20:25:25 -0300

    Aula 01 - Hello World

---
+++ hello.go
@@ -0,0 +1,20 @@
+// pacote principal
+package main
+
+// importando o pacote fmt p/ usar a função println
+import "fmt"
+
+// função principal
+func main() {
+
+	// imprimir hello world!
+	fmt.Println("Hello World!")
+	fmt.Println("Bem vindo ao curso de introdução à linguagem GO.")
+	fmt.Println("Bons Estudos!!")
+
+	// comando p/ gerar arquivo executável --> go build + nome_arquivo.go
+		// o arquivo deve estar salvo, vai gerar um arquivo .exe
+
+	// comando p/ executar o arquivo --> go run + nome_arquivo.go
+
+}
