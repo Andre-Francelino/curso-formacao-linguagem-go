ea4f3f461adaada4a1ef22c3c83cf11ccef91439
Andre-Francelino <107659656+Andre-Francelino@users.noreply.github.com>
2024-05-20 23:18:07 -0300

    Aulas - tipos de dados

---
+++ numero.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// tipo int
+	fmt.Println("1 + 1 =", 1+1)
+
+	// tipo float
+	fmt.Println("1.2 + 3.5 =", 1.2+3.5)
+
+}
---
+++ string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// sabendo o tamanho da string - conta quantos caracteres exitem incluindo os espaços
+	fmt.Println(len("Aula String GO!"))
+
+	// retornando o caractere na terceira posição da string (lendo e retornando o binário do caractere)
+	fmt.Println("Aula String GO!"[6])
+
+	// concatenando strings
+	fmt.Println("Aula " + "String " + "GO!")
+
+}
