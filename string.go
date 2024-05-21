61590818ad8f4ac71f4be7b9240b02e457ced077
Andre-Francelino <107659656+Andre-Francelino@users.noreply.github.com>
2024-05-21 06:52:48 -0300

    Update string.go

--- string.go
+++ string.go
@@ -7,7 +7,7 @@ func main() {
 	// sabendo o tamanho da string - conta quantos caracteres exitem incluindo os espaços
 	fmt.Println(len("Aula String GO!"))
 
-	// retornando o caractere na terceira posição da string (lendo e retornando o binário do caractere)
+	// retornando o caractere na posição da string (lendo e retornando o binário do caractere)
 	fmt.Println("Aula String GO!"[6])
 
 	// concatenando strings
